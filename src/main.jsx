c65aa1a294bb5ed6a47ab71bfea8b13bd9adf2b8
vonbaranow <zilkar@zvbpromo.com>
2024-09-01 12:34:13 -0300

    new project

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-// Import necessary libraries
-import 'bootstrap/dist/css/bootstrap.min.css'; // Import Bootstrap for styling
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx'; // Import the main App component
-import './index.css'; // Import custom CSS if needed
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-// Render the App component into the root element
 createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </StrictMode>,
+)
